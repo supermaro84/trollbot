01cce429312cc854dbd28382dd2be602973657b9
Unknown <tyrone.nowell@nibio.no>
2018-02-10 15:03:56 +0100

    Update arduino code
    Updated program structure and comments.
    Fixed inverted motor pwm pins

--- arduino/motor_controller.ino
+++ arduino/motor_controller.ino
@@ -1,234 +1,212 @@
 #include <ros.h>
-#include <geometry_msgs/Twist.h>
-#include <std_msgs/Int16.h>
-#include <std_msgs/Int32.h>
 #include <std_msgs/Int64.h>
 #include <std_msgs/Float32.h>
-#include <Servo.h>
-//////////////////////////////////////////////////////////////////////////////////////
-//Motor Pin definition
-//Left Motor
-#define USE_USBCOM
-#define MOTOR_L_1 12
-#define MOTOR_L_2 9
 
-//PWM 1 pin 
-#define ENA_PIN_1   3
+/* 
+** PINS
+*/
 
+/* Left motor pins for channel A */
+#define LEFT_DIR    12   /* Direction */
+#define LEFT_PWM    3    /* PWM */
+#define LEFT_BRK    9    /* Brake */
 
-//Right Motor
-#define MOTOR_R_1 13 
-#define MOTOR_R_2 8
+/* Right motor pins for channel B */
+#define RIGHT_DIR   13   /* Direction */
+#define RIGHT_PWM   11   /* PWM */
+#define RIGHT_BRK   8    /* Brake */
 
-//PWM 2 pin 
-#define ENB_PIN_2   11
-//#define RESET_PIN 4
+/* Left encoder pins */
+#define LEFT_ENC_A  4    /* Left encoder pin A */
+#define LEFT_ENC_B  5    /* Left encoder pin B */
 
-//encoder pins
-int left_encoder0PinA = 4;
- int left_encoder0PinB = 5;
+/* Right encoder pins */
+#define RIGHT_ENC_A  6   /* Right encoder pin A */
+#define RIGHT_ENC_B  7   /* Right encoder pin B */
 
- int lwheel = 0;
- int left_encoder0PinALast = LOW;
- int n = LOW;
+/*
+** GlOBAL VARIABLES
+*/
 
-//right encoder
-int right_encoder0PinA = 6;
- int right_encoder0PinB = 7;
-
- int rwheel = 0;
- int right_encoder0PinALast = LOW;
- int m = LOW;
-
-
+/* Motor status variables */
 float motor_left_speed = 0;
 float motor_right_speed = 0;
 
-ros::NodeHandle  nh;
+/* Left encoder variables */
+int lwheel = 0;
+int left_enc_A_last = LOW;
+int n = LOW;
 
-void left( const std_msgs::Float32& msg1){
+/* Right encoder variables */
+int rwheel = 0;
+int right_enc_A_last = LOW;
+int m = LOW;
+
+/* ROS node handle */
+ros::NodeHandle nh;
+
+/* Left wheel speed subscriber */
+void left(const std_msgs::Float32& msg1){
    motor_left_speed = msg1.data;
 }
+ros::Subscriber<std_msgs::Float32> sub1("left_wheel_speed", left); 
 
-void right( const std_msgs::Float32& msg2){
+/* Right wheel speed subscriber */
+void right(const std_msgs::Float32& msg2){
    motor_right_speed = msg2.data;
 }
-
-
-ros::Subscriber<std_msgs::Float32> sub1("left_wheel_speed", left); 
 ros::Subscriber<std_msgs::Float32> sub2("right_wheel_speed", right); 
 
-//left encoder
+/* Left encoder position publisher */
 std_msgs::Int64 lwheelMsg;
-ros::Publisher lwheelPub("lwheel", &lwheelMsg);   //self._Left_Encoder = rospy.Publisher('rwheel',Int64,queue_size = 10)
+ros::Publisher lwheelPub("lwheel", &lwheelMsg);
 
-//right encoder
+/* Right encoder position publisher */
 std_msgs::Int64 rwheelMsg;
-ros::Publisher rwheelPub("rwheel", &rwheelMsg);   //self._Right_Encoder = rospy.Publisher('rwheel',Int64,queue_size = 10)
+ros::Publisher rwheelPub("rwheel", &rwheelMsg);
 
 
 void setup()
 {
-  
-  nh.initNode();
-  nh.subscribe(sub1);
-nh.subscribe(sub2);
-  //Init Serial port with 115200 baud rate
-  Serial.begin(57600);  
-  
-  
-  //Setup Motors
-  SetupMotors();
- 
-
-//left encoder
- pinMode (left_encoder0PinA,INPUT);
-   pinMode (left_encoder0PinB,INPUT);
-  nh.initNode();
-  nh.advertise(lwheelPub);
+    /* Initialize node */
+    nh.initNode();
     
-//right encoder
-pinMode (right_encoder0PinA,INPUT);
-   pinMode (right_encoder0PinB,INPUT);
-  nh.initNode();
-  nh.advertise(rwheelPub);
-
- }
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//Setup Motors() function
-
-void SetupMotors()
-{
- 
- //Left motor
- pinMode(MOTOR_L_1, OUTPUT);
- pinMode(MOTOR_L_2, OUTPUT);
- 
-
- //Right Motor
-    pinMode(MOTOR_R_1, OUTPUT);
-    pinMode(MOTOR_R_2, OUTPUT);  
+    /* Subscribe to wheel speed topics */
+    nh.subscribe(sub1);
+    nh.subscribe(sub2);
+    
+    /* Publish to wheel position topics */
+    nh.advertise(rwheelPub);
+    nh.advertise(lwheelPub);
+    
+    /* Initialize serial port with a baud rate of 57600 */
+    Serial.begin(57600);
   
-}
+    /* Setup Motors */
+    /* Setup left motor */
+    pinMode(LEFT_DIR, OUTPUT);
+    pinMode(LEFT_BRK, OUTPUT);
 
+    /* Setup right motor */
+    pinMode(RIGHT_DIR, OUTPUT);
+    pinMode(RIGHT_BRK, OUTPUT);
+    
+    /* Setup encoders */
+    /* Setup left encoder */
+    pinMode(LEFT_ENC_A, INPUT);
+    pinMode(LEFT_ENC_B, INPUT);
+
+    /* Setup right encoder */
+    pinMode(RIGHT_ENC_A, INPUT);
+    pinMode(RIGHT_ENC_B, INPUT);
+}
 
 void loop()
 {
-
+    /* Process callbacks */
     nh.spinOnce();
     
-    //Update motor values with corresponding speed and send speed values through serial port
-    Update_Motors();
+    /* Update motor values with target speed */
+    update_motors();
 
-
-//left encoder 
- n = digitalRead(left_encoder0PinA);
-   if ((left_encoder0PinALast == LOW) && (n == HIGH)) {
-     if (digitalRead(left_encoder0PinB) == LOW) {
-       lwheel--;
-     } else {
-       lwheel++;
-     }
-lwheelMsg.data = lwheel;
-lwheelPub.publish(&lwheelMsg);
-   }
-   left_encoder0PinALast = n;
-
-
-//right encoder
-m = digitalRead(right_encoder0PinA);
-   if ((right_encoder0PinALast == LOW) && (m == HIGH)) {
-     if (digitalRead(right_encoder0PinB) == LOW) {
-       rwheel--;
-     } else {
-       rwheel++;
-     }
-rwheelMsg.data = rwheel;
-rwheelPub.publish(&rwheelMsg);
-   }
-   right_encoder0PinALast = m;
-
-
-  }
-
-
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-//Will update both motors
-void Update_Motors()
-{
-  
-  moveRightMotor();
-  moveLeftMotor();
-
- }
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-//Motor running function
-
-// to'g you have to change the pin values below as described - kishore
-void moveRightMotor()
-{
-  if (motor_right_speed>0)
-  {
- //right wheel forward (change the pin values accordingly)   - kishore    
- digitalWrite(MOTOR_R_1, LOW);
- digitalWrite(MOTOR_R_2, LOW);
- analogWrite(ENA_PIN_1,-1*motor_right_speed);
-    
-  }
-  else if(motor_right_speed<0)
-  {
-
-//right wheel backward (change the pin values accordingly) - kishore
- digitalWrite(MOTOR_R_1, HIGH);
- digitalWrite(MOTOR_R_2, LOW);
- analogWrite(ENA_PIN_1,motor_right_speed);
- 
-  }
-  
-  else if(motor_right_speed == 0)
-  {
-
-//Right wheel stop (change the pin values accordingly) - kishore
- digitalWrite(MOTOR_R_1,HIGH);
- digitalWrite(MOTOR_R_2,HIGH);
-    
-    
-  }
+    /* Send encoder positions through serial port */
+    read_encoders();
 }
 
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-void moveLeftMotor()
+/* Update motor speeds */
+void update_motors()
 {
- if (motor_left_speed > 0)
-  {
- //left wheel forward (change the pin values accordingly)   - kishore    
-digitalWrite(MOTOR_L_1, HIGH);
-digitalWrite(MOTOR_L_2, LOW);
-analogWrite(ENB_PIN_2,-1*motor_left_speed);
-  }
-  else if(motor_left_speed < 0)
-  {
-
-//left wheel backward (change the pin values accordingly) - kishore
- digitalWrite(MOTOR_L_1, LOW);
- digitalWrite(MOTOR_L_2, LOW);
- analogWrite(ENB_PIN_2,motor_left_speed);
-
-  }
-  else if(motor_left_speed == 0)
-  {
-//left wheel stop (change the pin values accordingly) - kishore
-   digitalWrite(MOTOR_L_1,HIGH);
-   digitalWrite(MOTOR_L_2,HIGH);
-  
-   }  
-  
-  
+    move_right_motor();
+    move_left_motor();
 }
 
+/* Update right motor speed */
+void move_right_motor()
+{
+    if (motor_right_speed > 0)
+    {
+        /* Right wheel forward */
+        digitalWrite(RIGHT_DIR, LOW);
+        digitalWrite(RIGHT_BRK, LOW);
+        analogWrite(RIGHT_PWM, -motor_right_speed);
+    } 
+    else if(motor_right_speed < 0)
+    {
+        /* Right wheel backward */
+        digitalWrite(RIGHT_DIR, HIGH);
+        digitalWrite(RIGHT_BRK, LOW);
+        analogWrite(RIGHT_PWM, motor_right_speed);
+    }
+    else if(motor_right_speed == 0)
+    {
+        /* Right wheel stop */
+        digitalWrite(RIGHT_DIR, HIGH);
+        digitalWrite(RIGHT_BRK, LOW);
+        analogWrite(RIGHT_PWM, 0);
+    }
+}
 
+/* Update left motor speed */
+void move_left_motor()
+{
+    if (motor_left_speed > 0)
+    {
+        /* Left wheel forward */
+        digitalWrite(LEFT_DIR, HIGH);
+        digitalWrite(LEFT_BRK, LOW);
+        analogWrite(LEFT_PWM, -motor_left_speed);
+    }
+    else if(motor_left_speed < 0)
+    {
+        /* Left wheel backward */
+        digitalWrite(LEFT_DIR, LOW);
+        digitalWrite(LEFT_BRK, LOW);
+        analogWrite(LEFT_PWM, motor_left_speed);
+    }
+    else if(motor_left_speed == 0)
+    {
+        /* Left wheel stop */
+        digitalWrite(LEFT_DIR, HIGH);
+        digitalWrite(LEFT_BRK, LOW);
+        analogWrite(LEFT_PWM, 0);
+    }    
+}
 
+/* Read both wheel encoders */
+void read_encoders()
+{
+    read_left_encoder();
+    read_right_encoder();
+}
+
+/* Read left encoder */
+void read_left_encoder()
+{
+    n = digitalRead(LEFT_ENC_A);
+    if ((left_enc_A_last == LOW) && (n == HIGH)) {
+        if (digitalRead(LEFT_ENC_B) == LOW) {
+            lwheel--;
+        } else {
+            lwheel++;
+        }
+        lwheelMsg.data = lwheel;
+        lwheelPub.publish(&lwheelMsg);
+    }
+    left_enc_A_last = n;
+}
+
+/* Read right encoder */        
+void read_right_encoder()
+{
+    m = digitalRead(RIGHT_ENC_A);
+    if ((right_enc_A_last == LOW) && (m == HIGH)) {
+        if (digitalRead(RIGHT_ENC_B) == LOW) {
+            rwheel--;
+        } else {
+            rwheel++;
+        }
+        rwheelMsg.data = rwheel;
+        rwheelPub.publish(&rwheelMsg);
+    }
+    right_enc_A_last = m;
+}
